2472520c045e2e154a47dd613db326254c23b1c7
unknown <ITA7-TN06@drheuer.net>
2025-06-27 10:43:40 +0200

    added (dummy) api endpoint for fetching devices, added cors

--- backend/index.js
+++ backend/index.js
@@ -4,14 +4,43 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const fastify_1 = __importDefault(require("fastify"));
+const cors_1 = __importDefault(require("@fastify/cors"));
 const server = (0, fastify_1.default)();
-server.get('/ping', async (request, reply) => {
-    return 'pong\n';
+server.register(cors_1.default, {
+    origin: "http://localhost:5173",
 });
-server.listen({ port: 8080 }, (err, address) => {
-    if (err) {
-        console.error(err);
+server.get("/devices", async (request, reply) => {
+    return [
+        {
+            name: "Dell XPS1 Server",
+            ip: "192.168.2.1",
+            os: "Ubuntu Server",
+            room: "Room 411.2",
+            status: "online",
+        },
+        {
+            name: "HP ProLiant DL380",
+            ip: "192.168.2.2",
+            os: "Windows Server 2019",
+            room: "Room 412.1",
+            status: "offline",
+        },
+        {
+            name: "Lenovo ThinkSystem SR650",
+            ip: "192.168.2.3",
+            os: "CentOS 8",
+            room: "Room 413.3",
+            status: "online",
+        },
+    ];
+});
+const start = async () => {
+    try {
+        await server.listen({ port: 8080 });
+    }
+    catch (err) {
+        server.log.error(err);
         process.exit(1);
     }
-    console.log(`Server listening at ${address}`);
-});
+};
+start();
--- backend/index.ts
+++ backend/index.ts
@@ -1,15 +1,52 @@
-import fastify from 'fastify'
+import fastify from "fastify";
+import cors from "@fastify/cors";
 
-const server = fastify()
+const server = fastify();
+server.register(cors, {
+  origin: "http://localhost:5173",
+});
 
-server.get('/ping', async (request, reply) => {
-  return 'pong\n'
-})
+interface Device {
+  name: string;
+  ip: string;
+  os: string;
+  room: string;
+  status: string;
+}
 
-server.listen({ port: 8080 }, (err, address) => {
-  if (err) {
-    console.error(err)
-    process.exit(1)
+server.get("/devices", async (request, reply): Promise<Device[]> => {
+  return [
+    {
+      name: "Dell XPS1 Server",
+      ip: "192.168.2.1",
+      os: "Ubuntu Server",
+      room: "Room 411.2",
+      status: "online",
+    },
+    {
+      name: "HP ProLiant DL380",
+      ip: "192.168.2.2",
+      os: "Windows Server 2019",
+      room: "Room 412.1",
+      status: "offline",
+    },
+    {
+      name: "Lenovo ThinkSystem SR650",
+      ip: "192.168.2.3",
+      os: "CentOS 8",
+      room: "Room 413.3",
+      status: "online",
+    },
+  ];
+});
+
+const start = async (): Promise<void> => {
+  try {
+    await server.listen({ port: 8080 });
+  } catch (err) {
+    server.log.error(err);
+    process.exit(1);
   }
-  console.log(`Server listening at ${address}`)
-})
+};
+
+start();
